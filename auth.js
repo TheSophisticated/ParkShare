5e6c9e465803dd8ee950d5db7959bbfb7cfa7e2e
Ishaan Pandey <ipgurgaon2@gmail.com>
2026-01-26 20:18:02 +0530

    Adds all Basic Auth Features (Signup, Signout, Login)

--- auth.js
+++ auth.js
@@ -5,30 +5,67 @@ const supabase = createClient(
   'sb_publishable_SSZqqiwFwnPispVtOBqbpg_gr01dRlX'
 )
 
-const signupForm = document.getElementById("login-form");
+//Fetches the Sign-Up form from the DOM
+const signupForm = document.getElementById("sign-up");
 signupForm.addEventListener('submit', signup);
 
+const signInForm = document.getElementById("sign-in");
+signInForm.addEventListener('submit', signIn);
+
+const signoutBtn = document.getElementById("signout-btn");
+signoutBtn.addEventListener('click', signout);
+
+//Get Email and Password from Input fields and make User
 async function signup(event){
   event.preventDefault();
-  const user_input =  document.getElementById('email');
-  const pwd_input =  document.getElementById('password');
+  const email_input =  document.getElementById('email').value;
+  const username_input = document.getElementById('username').value;
+  const pwd_input =  document.getElementById('password').value;
 
-  const email = user_input.value;
-  const pwd = pwd_input.value;
+  const { data, error } = await supabase.auth.signUp({
+    email: email_input,
+    password: pwd_input,
+    options: {
+      data : {
+        display_name: username_input,
+      }
+    }
+  })
 
-  const userDetails = {
-    email: email,
-    password: pwd
-  };
+  if(error){
+    console.log("Something Went Wrong!", error.message);
+  }
+  else{
+    alert("Account Made Successfully!");
+  }
+}
 
-  console.log(email);
-  console.log(pwd);
+async function signIn(event) {
+  
+  event.preventDefault();
+  const email_input =  document.getElementById('signIn-email').value;
+  const pwd_input =  document.getElementById('signIn-password').value;
+  
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email_input,
+    password: pwd_input,
+  })
 
-  const{data, error} = await supabase.auth.signUp({
-    body: userDetails,
-  });
-
-  console.log("Gello");
+  if(error){
+    console.log("Something Went Wrong!", error.message);
+  }
+  else{
+    alert("Signed In Successfully!");
+  }
 
 }
 
+async function signout(event){
+  const { error } = await supabase.auth.signOut();
+  if(error){
+    console.log("Something Went Wrong!", error.message);
+  }
+  else{
+    alert("Signed Out Successfully!");
+  }
+}
